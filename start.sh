31ce584b2024282d4a3904336a7c65419b6282b9
Jacek Adamczyk <jacek@adamczyk.me>
2025-09-20 23:05:36 +0200

    record video

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/fsspx/.pyenv/shims/python audio_recorder.py -u -d 1 2> $HOME/`date +\%Y\%m\%d\%H\%M\%S`-cron.error.log
+/home/fsspx/.pyenv/shims/python audio_recorder.py -u -c -d 1 2> $HOME/`date +\%Y\%m\%d\%H\%M\%S`-cron.error.log
